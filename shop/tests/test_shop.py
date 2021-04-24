18582df57aa6728390a40dc6d00fe9c7e09201be
Jesse De Loore <jesse@sennac.be>
2021-04-24 13:54:51 +0200

    Fixed issue with vat calculation Made cart more clear added subtotal for 1 recipe

--- shop/tests/test_shop.py
+++ shop/tests/test_shop.py
@@ -23,12 +23,15 @@ def test_recipe_price(basic_order: OrderRecipe):
     assert ingredients[2].ingredient.vat_pct == 6
     assert ingredients[2].ingredient.sell_price == 4.5
     assert basic_order.order.total_price == basic_order.total_price
+    assert basic_order.order.total_vat == basic_order.total_vat
+    assert pytest.approx(basic_order.order.total_price_no_vat,
+                         1e-4) == basic_order.total_price - basic_order.total_vat
     assert basic_order.amount_multiplier == 1
+    assert basic_order.order.show_vat
     price = 1.06 * (2.5 * 0.05 + 3.5 * 0.07 + 4.5 * 0.13)
     vat = 0.06 * (2.5 * 0.05 + 3.5 * 0.07 + 4.5 * 0.13)
-    assert pytest.approx(float(basic_order.total_price), 0.00001) == price
-    assert pytest.approx(float(basic_order.total_vat), 0.00001) == vat
-    assert pytest.approx(float(basic_order.total_price), 0.00001) == vat
+    assert pytest.approx(float(basic_order.total_price), 1e-6) == price
+    assert pytest.approx(float(basic_order.total_vat), 1e-6) == vat
 
 
 @pytest.mark.django_db
@@ -38,9 +41,13 @@ def test_recipe_price_with_multiplier(basic_order: OrderRecipe):
                              add_to_recipe_ingredients=[("banaan", "Fruitpap")])
     # vat * sum(ingredient_amount * price * multipliers)
     price = 1.06 * (1.3 * 2.5 * 0.05 + 3.5 * 0.07 + 4.5 * 0.13)
-    assert pytest.approx(float(basic_order.total_price), 0.00001) == price
+    assert pytest.approx(float(basic_order.total_price), 1e-6) == price
     s = StorageMethodFactory(name="Vacuum", labour_multiplier=1.1,
                              add_to_recipe_ingredients=[("banaan", "Fruitpap"),
                                                         ("kiwi", "Fruitpap")])
     price = 1.06 * ((1 + 0.3 + 0.1) * 2.5 * 0.05 + 1.1 * 3.5 * 0.07 + 4.5 * 0.13)
-    assert pytest.approx(float(basic_order.total_price), 0.00001) == price
+    assert basic_order.order.total_price == basic_order.total_price
+    assert basic_order.order.total_vat == basic_order.total_vat
+    assert pytest.approx(basic_order.order.total_price_no_vat,
+                         1e-6) == basic_order.total_price - basic_order.total_vat
+    assert pytest.approx(float(basic_order.total_price),1e-6) == price
