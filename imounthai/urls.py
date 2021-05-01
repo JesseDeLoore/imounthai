f2267521e46733aa3a885d3c91ed2990dcda9589
Jesse De Loore <jesse@sennac.be>
2021-05-01 16:59:45 +0200

    Add page order history Add functionality to "Bestel" button in cart Add Order again buttons to order history Add endpoint to add recipe to order Reformat with Black

--- imounthai/urls.py
+++ imounthai/urls.py
@@ -10,16 +10,22 @@ from wagtail.documents import urls as wagtaildocs_urls
 
 from home.views import UserAccountCreateView
 from search import views as search_views
-from shop.views import shopping_cart, RecipeRemoveView, RecipeUpdateView, \
-    create_new_temp_recipe, OrderRecipeUpdateView
+from shop.views import (
+    shopping_cart,
+    order_history,
+    RecipeRemoveView,
+    RecipeUpdateView,
+    create_new_temp_recipe,
+    OrderRecipeUpdateView,
+    OrderUpdateView,
+    OrderRecipeCreateView,
+)
 
 urlpatterns = [
-    path('django-admin/', admin.site.urls),
-
-    path('admin/', include(wagtailadmin_urls)),
-    path('documents/', include(wagtaildocs_urls)),
-    path('search/', search_views.search, name='search'),
-
+    path("django-admin/", admin.site.urls),
+    path("admin/", include(wagtailadmin_urls)),
+    path("documents/", include(wagtaildocs_urls)),
+    path("search/", search_views.search, name="search"),
 ]
 
 if settings.DEBUG:
@@ -34,28 +40,70 @@ urlpatterns += [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('verification/', include('verify_email.urls')),
-    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-
-    path('password_reset/', auth_views.PasswordResetView.as_view(html_email_template_name="registration/html_password_reset_email.html"), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
-    path('create_account/', UserAccountCreateView.as_view(), name="create_account"),
-
-    path('shop/cart/', shopping_cart, name="shopping_cart"),
-    path('shop/order/create_temp_recipe/<int:order>/<int:recipe>', create_new_temp_recipe, name="create_temp_recipe"),
-    path('shop/order/update_temp_recipe/<int:pk>', RecipeUpdateView.as_view(), name="update_temp_recipe"),
-    path('shop/order/remove_recipe/<int:pk>', RecipeRemoveView.as_view(), name="remove_recipe"),
-    path('shop/order/set_order_amount/<int:pk>', OrderRecipeUpdateView.as_view(), name="update_order_quant"),
-    path('social', include('social_django.urls', namespace='social')),
-    path('cookies/', include('cookie_consent.urls')),
-    path('django-admin/django-ses/', include('django_ses.urls')),
-
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("verification/", include("verify_email.urls")),
+    path(
+        "password_change/",
+        auth_views.PasswordChangeView.as_view(),
+        name="password_change",
+    ),
+    path(
+        "password_change/done/",
+        auth_views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(
+            html_email_template_name="registration/html_password_reset_email.html"
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
+    path("create_account/", UserAccountCreateView.as_view(), name="create_account"),
+    path("shop/cart/", shopping_cart, name="shopping_cart"),
+    path("shop/cart/add/<int:pk>", OrderRecipeCreateView.as_view(), name="add_to_cart"),
+    path(
+        "shop/order/create_temp_recipe/<int:order>/<int:recipe>",
+        create_new_temp_recipe,
+        name="create_temp_recipe",
+    ),
+    path(
+        "shop/order/update_temp_recipe/<int:pk>",
+        RecipeUpdateView.as_view(),
+        name="update_temp_recipe",
+    ),
+    path(
+        "shop/order/remove_recipe/<int:pk>",
+        RecipeRemoveView.as_view(),
+        name="remove_recipe",
+    ),
+    path(
+        "shop/order/set_order_amount/<int:pk>",
+        OrderRecipeUpdateView.as_view(),
+        name="update_order_quant",
+    ),
+    path(
+        "shop/order/confirm/<int:pk>", OrderUpdateView.as_view(), name="confirm_order",
+    ),
+    path("shop/order/history", order_history, name="order_history"),
+    path("social", include("social_django.urls", namespace="social")),
+    path("cookies/", include("cookie_consent.urls")),
+    path("django-admin/django-ses/", include("django_ses.urls")),
     path("", include(wagtail_urls)),
-
 ]
