82f789626673bb76ccc98efd1e3e79a7b6690cde
Jesse De Loore <jesse@sennac.be>
2021-09-16 21:26:11 +0200

    - prevent error when ingredient is not set on temp recipe form

--- shop/forms.py
+++ shop/forms.py
@@ -33,6 +33,8 @@ class IngredientForm(forms.ModelForm):
         self.fields["ingredient"].queryset = Ingredient.objects.filter(is_available=True)
         self.helper = FormHelper()
         self.helper.form_tag = False
+        if not self.instance.ingredient:
+            return
         if self.instance.ingredient.price_unit is not None:
             self.fields.pop("amount_units")
         if self.instance.ingredient.price_unit not in ("g", "kg"):
